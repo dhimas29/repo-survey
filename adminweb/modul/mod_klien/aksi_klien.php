683ed8987f443242b07382292acb17bb50c08498
Dhimas <44453057+dhimas29@users.noreply.github.com>
2022-03-13 22:18:46 +0700

    Update aksi_klien.php

--- adminweb/modul/mod_klien/aksi_klien.php
+++ adminweb/modul/mod_klien/aksi_klien.php
@@ -30,13 +30,13 @@ elseif($module=='klien' AND $act=='up'){
 			$mail->isSMTP();                                            // Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-			$mail->Username   = 'skripsi2021dela@gmail.com';                     // SMTP username
-			$mail->Password   = 'skripsi21dela';                               // SMTP password
+			$mail->Username   = '';                     // SMTP username
+			$mail->Password   = '';                               // SMTP password
 			$mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 			//Recipients
-			$mail->setFrom('skripsi2021dela@gmail.com', 'Dela');
+			$mail->setFrom('', '');
 			$mail->addAddress($email);     // Add a recipient
 
 			// Content
